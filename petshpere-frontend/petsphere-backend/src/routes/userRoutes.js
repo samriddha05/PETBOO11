a37023f8a7d85f437cddfcf9cf387be6b54ed30a
sarthakmishra1410 <sarthakmishra1277@gmail.com>
2026-05-14 11:52:24 +0000

    Delete all files in main-final branch

--- petshpere-frontend/petsphere-backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { syncUser } = require("../controllers/userController");
-
-const router = express.Router();
-
-router.post("/sync", syncUser);
-
-module.exports = router;
